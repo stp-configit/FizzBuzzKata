89fea66a6f2b01503b4002fdb01e26041ac80ad1
Stefan Popescu <stefan@configit.com>
2016-12-07 11:07:19 +0000

    initial FizzBuzz project

---
+++ TDDKata/TDDKata/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿namespace TDDKata {
+  public class FizzBuzz {
+
+    // Print numbers from 1 to 100. 
+    // But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+    // For numbers which are multiples of both three and five print "FizzBuzz".
+    public string PrintFizzbuzz() {
+      return null;
+    }
+  }
+}
